a456cd07b4f4f37fe07f89f00480398c02411d2d
John Edwards <joedw1978@gmail.com>
2019-06-17 15:13:28 -0400

    changes

--- client/src/app/components/ping/ping.component.ts
+++ client/src/app/components/ping/ping.component.ts
@@ -4,7 +4,7 @@ import * as Peer from "peerjs_fork_firefox40";
 @Component({
   selector: 'app-ping',
   template: `
-   <span [ngStyle]="{'color': people[socket.id].color}">{{people | json}}</span>
+   {{people[socket.id]}}
   `
 })
 export class PingComponent {
