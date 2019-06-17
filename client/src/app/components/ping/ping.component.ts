e40cf48c03c94dbd4eec156cea5204a3034f526f
John Edwards <joedw1978@gmail.com>
2019-06-17 19:44:05 -0400

    changes

--- client/src/app/components/ping/ping.component.ts
+++ client/src/app/components/ping/ping.component.ts
@@ -50,6 +50,7 @@ export class PingComponent {
     ) {
       this.device = "mobile";
     }
+    console.log(this.name, this.device)
     this.socket.emit('send name', {name: this.name, device: this.device})
   }
 
