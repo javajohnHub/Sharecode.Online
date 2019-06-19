618198912167f1522a8bf0ed8ea0577e977d59f1
John Edwards <joedw1978@gmail.com>
2019-06-19 19:21:11 -0400

    changes

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -269,68 +269,25 @@ module.exports = io => {
         time: d.getHours() + ":" + d.getMinutes()
       });
     });
-    // socket.on("disconnected", () => {
-    //   let rms = Object.values(rooms);
-    //   rms.forEach((room) => {
-    //     if(_.contains(room.people, socket.id)){
-    //       room.people = _.without(room.people, socket.id);
-    //       if(room.owner == socket.id){
-    //         delete rooms[room.id]
-    //       }
-    //     }
+    socket.on("disconnected", () => {
+      let rms = Object.values(rooms);
+      rms.forEach((room) => {
+        if(_.contains(room.people, socket.id)){
+          room.people = _.without(room.people, socket.id);
+          if(room.owner == socket.id){
+            delete rooms[room.id]
+          }
+        }
 
-    //   })
+      })
 
-    //   delete people[socket.id];
-    //   peopleCount = _.size(people);
-    //   io.sockets.emit("update-people", { people, peopleCount });
-    //   let roomCount = _.size(rooms);
-    //   io.sockets.emit("update-rooms", { rooms, roomCount });
-    // });
+      delete people[socket.id];
+      peopleCount = _.size(people);
+      io.sockets.emit("update-people", { people, peopleCount });
+      let roomCount = _.size(rooms);
+      io.sockets.emit("update-rooms", { rooms, roomCount });
+    });
     socket.on("disconnect", () => {
-      console.log(rooms)
-      if(rooms[people[socket.id]]){
-        let room = rooms[people[socket.id].inroom]
-        if (socket.id === room.owner) {
-          let d = new Date();
-          io.sockets.in(socket.room).emit("admin chat", {
-            from: "Admin",
-            msg:
-              "The owner (" +
-              people[socket.id].name +
-              ") has left the room. The room is removed and you have been disconnected from it as well.",
-            color: adminColor,
-            time: d.getHours() + ":" + d.getMinutes()
-          });
-          let socketids = [];
-        for (let i = 0; i < sockets.length; i++) {
-          socketids.push(sockets[i].id);
-          if ((_.contains(socketids), room.people)) {
-            sockets[i].leave(room.name);
-          }
-        }
-
-        if ((_.contains(room.people), socket.id)) {
-          for (let i = 0; i < room.people.length; i++) {
-            if (people[room.people[i]]) {
-
-              people[room.people[i]].inroom = null;
-
-            }
-          }
-        }
-        delete rooms[people[socket.id].owns];
-         delete people[socket.id]
-
-          room.people = _.without(room.people, socket.id); //remove people from the room:people{}collection
-          peopleCount = _.size(people);
-          io.sockets.emit("update-people", { people, peopleCount });
-          let roomCount = _.size(rooms);
-          io.sockets.emit("update-rooms", { rooms, roomCount });
-        }
-      }
-
-
 
     });
   });
