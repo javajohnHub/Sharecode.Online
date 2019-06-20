119dd33f5e473950ab9c5fbc0b214295322586a2
John Edwards <joedw1978@gmail.com>
2019-06-19 20:28:59 -0400

    changes

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -272,20 +272,18 @@ module.exports = io => {
       });
     });
     socket.on("disconnected", () => {
-     rooms.forEach((room) => {
       let socketids = [];
       for (let i = 0; i < sockets.length; i++) {
         socketids.push(sockets[i].id);
         if ((_.contains(socketids), room.people)) {
           sockets[i].leave(room.name);
+          console.log(sockets[i])
         }
+      }
+      for (let i = 0; i < room.people.length; i++) {
+        people[room.people[i]].inroom = null;
       }
 
-      if ((_.contains(room.people), socket.id)) {
-        for (let i = 0; i < room.people.length; i++) {
-          people[room.people[i]].inroom = null;
-        }
-      }
       delete rooms[people[s.id].owns];
       people[socket.id].owns = null;
       room.people = _.without(room.people, socket.id); //remove people from the room:people{}collection
@@ -293,7 +291,7 @@ module.exports = io => {
       io.sockets.emit("update-people", { people, peopleCount });
       let roomCount = _.size(rooms);
       io.sockets.emit("update-rooms", { rooms, roomCount });
-     })
+
 
       // let rms = Object.values(rooms);
       // rms.forEach(room => {
