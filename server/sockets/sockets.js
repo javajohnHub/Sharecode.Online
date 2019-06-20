08651243f1e759e6abed7d31a4b499584677823a
John Edwards <joedw1978@gmail.com>
2019-06-19 20:52:54 -0400

    changes

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -282,7 +282,7 @@ module.exports = io => {
           if (room.owner == socket.id) {
             console.log(people[rooms[room.id].people[0]])
             people[rooms[room.id].people[0]].owns = room.id;
-            rooms[room.id].owner = people[socket.id]
+            rooms[room.id].owner = socket.id
           } else {
             let socketids = [];
             for (let i = 0; i < sockets.length; i++) {
