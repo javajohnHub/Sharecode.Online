d45886b8d69fae55ef3beca2b8380e73189c937f
John Edwards <joedw1978@gmail.com>
2019-06-23 13:06:00 -0400

    changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,17 +12,23 @@ import {routing} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoContentComponent } from './components/no-content/no-content';
 import { PingComponent } from './components/ping/ping.component';
-
+import { NameComponent } from './components/name/name.component';
+import {ToastModule} from 'primeng/toast';
+import {DialogModule} from 'primeng/dialog';
 @NgModule({
   declarations: [
     AppComponent,
     NoContentComponent,
-    PingComponent
+    PingComponent,
+    NameComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    ToastModule,
+    DialogModule,
     routing
   ],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
---
+++ client/src/app/components/name/name.component.ts
@@ -0,0 +1,56 @@
+import { Component, HostListener } from "@angular/core";
+import { SocketService } from "../../shared/socket.service";
+import {MessageService} from 'primeng/api';
+import * as Peer from "peerjs_fork_firefox40";
+@Component({
+  selector: "app-name",
+  templateUrl: `./name.component.html`
+})
+export class NameComponent {
+  socket: any;
+  peer;
+  name;
+  device;
+  peerId;
+  nameDialogVis = true;
+  constructor(private messageService: MessageService) {
+    this.socket = SocketService.getInstance();
+    this.peer = new Peer({
+      host: "sharecode.online",
+      port: "9000",
+      path: "/",
+      secure: true,
+      debug: 3
+    });
+    this.peer.on("open", () => {
+      this.peerId = this.peer.id;
+      this.socket.emit("peerId", this.peer.id);
+
+      this.socket.on("exists", proposedName => {
+        this.name = proposedName.proposedName;
+        this.nameDialogVis = true;
+        this.messageService.add({severity:'warning', summary:'Name already exists', detail:'Try ' + this.name});
+      });
+
+    });
+  }
+
+  @HostListener("window:beforeunload", ["$event"])
+  unloadHandler(event: Event) {
+    this.socket.emit("disconnected");
+  }
+
+  sendName() {
+    this.device = "desktop";
+    if (
+      navigator.userAgent.match(
+        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i
+      )
+    ) {
+      this.device = "mobile";
+    }
+    this.socket.emit("send name", { name: this.name, device: this.device });
+    this.nameDialogVis = false;
+  }
+
+}
