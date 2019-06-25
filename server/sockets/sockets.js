522a00bedf20332d8ae1c883ebf4e1660a58da46
John Edwards <joedw1978@gmail.com>
2019-06-25 15:54:55 -0400

    changes

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -364,7 +364,7 @@ module.exports = io => {
     });
     socket.on("call rejected", data => {
       console.log(data)
-      io.sockets.connected[data.to].emit("call rejected", {});
+      io.sockets.connected[data.callee.id].emit("call rejected", {});
     });
 
     socket.on("disconnect", () => {
