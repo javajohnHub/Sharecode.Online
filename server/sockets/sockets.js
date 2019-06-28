2aceef1d5c24d2ede5c0abeb731adabd88903ea6
John Edwards <joedw1978@gmail.com>
2019-06-28 11:40:46 -0400

    changes

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -267,12 +267,13 @@ module.exports = io => {
 
     chatHandler = (msg) => {
       msg = msg.split('/')[1].toLowerCase()
-      switch(msg){
+      msg = msg.split(' ')
+      switch(msg[0]){
         case 'btc':
             request('https://blockchain.info/ticker', function (error, response, body) {
               console.log('error:', error); // Print the error if one occurred and handle it
               console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-              console.log(body)
+              console.log(body[msg[1]])
         });
         break;
       }
