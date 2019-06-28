05925174fa1fb888e529e7ad621bcf52e134f366
John Edwards <joedw1978@gmail.com>
2019-06-28 11:45:44 -0400

    changes

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -274,7 +274,7 @@ module.exports = io => {
             request('https://blockchain.info/ticker', function (error, response, body) {
               console.log('error:', error); // Print the error if one occurred and handle it
               console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-              console.log(body)
+              console.log(body[msgs[1].toUpperCase()])
         });
         break;
       }
