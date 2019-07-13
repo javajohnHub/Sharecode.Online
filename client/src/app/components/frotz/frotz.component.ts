09baf90c99ad8c05fa7f08d65488249f77485252
John Edwards <joedw1978@gmail.com>
2019-07-13 12:13:22 -0400

    changes

--- client/src/app/components/frotz/frotz.component.ts
+++ client/src/app/components/frotz/frotz.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {SocketService} from '../../shared/socket.service';
 import {SelectItem} from 'primeng/api';
+import * as uuid from 'uuid';
 @Component({
   selector: 'app-frotz',
   template: `
@@ -8,6 +9,8 @@ import {SelectItem} from 'primeng/api';
     <button pButton type="button" label="Clear"(click)="clear()"></button>
     <div *ngIf="game_data" [innerHTML]="game_data"></div><br/>
     <input type="text" (keydown.enter)="send()" pInputText [(ngModel)]="command"/><button pButton type="button" label="Send"  (click)="send()"></button>
+    <button pButton type="button" label="Save"  (click)="save()"></button>
+    <button pButton type="button" label="Load Game"  (click)="load()"></button>
   `
 })
 export class FrotzComponent {
@@ -16,12 +19,14 @@ export class FrotzComponent {
   chosen_game: string;
   game_data = '';
   command: string;
+  myId;
   constructor() {
     this.socket = SocketService.getInstance();
     this.socket.emit('get games');
   }
 
   ngOnInit(){
+    this.myId = uuid.v4();
     this.socket.on('send games', (games) => {
       games.forEach(game => {
         this.games.push({label: game.split('.')[0], value: game.split('.')[0] })
@@ -40,11 +45,20 @@ export class FrotzComponent {
  }
  clear(){
    this.game_data = '';
-   this.socket.emit('command', 'brief')
+   this.socket.emit('command', 'L')
  }
 
  send(){
    this.socket.emit('command', this.command)
    this.command = '';
+ }
+
+ save(){
+  this.socket.emit('command', 'save');
+  localStorage.setItem('id', this.myId)
+  this.socket.emit('command', 'save/' + this.chosen_game + '_' + this.myId + '.sav')
+ }
+ load(){
+  this.socket.emit('command', 'restore');
  }
 }
