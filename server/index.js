330cdb782234ffb21b8ccada58db26627c292703
John Edwards <joedw1978@gmail.com>
2019-08-18 13:01:25 -0400

    changes

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ var url = "mongodb://127.0.0.1:27017/sharecode";
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect(url);
- var Blog = require('./models/blog.model');
-console.log(new Blog());
+
 // Certificate
 const privateKey = fs.readFileSync(
   "/etc/letsencrypt/live/sharecode.online/privkey.pem",
